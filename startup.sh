3b8601e5a994bb41e7be431e0d9ca245b09056b4
dpryan79 <dpryan79@gmail.com>
2015-06-29 15:09:57 +0200

    apache2 doesn't want to follow the links

--- startup.sh
+++ startup.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
 cd /var/www/html/db
-if [ -f *.html ] then;
+#.html files
+if [ -f *.html ]
+then
     for html in `ls *.html`
     do
         rm /var/www/html/$html
-        ln -s $html /var/www/html/$html
+        cp $html /var/www/html/$html
     done
 fi
 
-if [ -f /var/www/html/db/group ] then;
+#.rc files
+if [ -f *.rc ]
+then
+    for rc in `ls *.rc`
+    do
+        rm /var/www/html/$rc
+        cp $rc /var/www/html/$rc
+    done
+fi
+
+#/etc/group
+if [ -f /var/www/html/db/group ]
+then
     rm /etc/group
     ln -s /var/www/html/db/group /etc/group
 fi
 
+a2enmod cgi
 service apache2 start
 
-tail -f /var/log/apache2/*.log
+tail -f /var/log/apache2/*.log /var/www/html/blast/wwwblast.log
